d81ea4c97384df1b7dd2a2481a2e88ecfbf28551
marc.luettecke <marc.luettecke@uni-konstanz.de>
2020-04-05 19:38:51 +0200

    inital file commitment of course material

---
+++ week1/Programming Challenges/Maximum Pairwise Product/maximum_pairwise_product.py
@@ -0,0 +1,39 @@
+# python3
+from functools import reduce
+from operator import mul
+
+
+def max_pairwise_product_naive(numbers):
+    """
+
+    """
+    assert len(numbers) >= 2
+    assert all(0 <= x <= 2 * 10 ** 5 for x in numbers)
+
+    product = 0
+
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            product = max(product, numbers[i] * numbers[j])
+
+    return product
+
+
+
+def max_pairwise_product(numbers):
+    """
+    Method to return the product of the two largest numbers of array in linear time.
+    Args:
+        numbers: input of numbers
+
+    Returns:
+        int: product of two largest elemens of array
+    """
+    return reduce(mul, sorted([int(x) for x in numbers])[-2:], 1)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    input_numbers = list(map(int, input().split()))
+    assert len(input_numbers) == n
+    print(max_pairwise_product(input_numbers))
